fa3405239ba24d1dde3dda703b1d25a2c9ad983f
wjm <1186619054@qq.com>
2018-06-04 09:48:23 +0800

    2018-06-04逻辑调整

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,7 @@
     var vm = new Vue({
         el: '#app',
         data: {
-            showIndex: true,
-            showLogin: false,
+            showEditArea: true,
             actionType: 'login',
             user: {
                 name: '',
@@ -22,17 +21,66 @@
                 blogLink: 'https://wjmnightingale.github.io/wjmBlog.github.io',
                 githubLink: 'https://github.com/WjmNightingale',
                 jobTitle: '前端工程师',
+                skills: [{
+                        name: '技能1',
+                        description: '技能1非常厉害的'
+                    },
+                    {
+                        name: '技能2',
+                        description: '技能2非常厉害的'
+                    },
+                    {
+                        name: '技能3',
+                        description: '技能3非常厉害的'
+                    },
+                    {
+                        name: '技能4',
+                        description: '技能4非常厉害的'
+                    }
+                ],
+                projects: [
+                    {
+                        name: 'Canvas轻画板1',
+                        keywords: 'Canvas、jQuery、移动端',
+                        description: '一个轻量级画板',
+                        previewLink: '***这里是预览链接***',
+                        codeLink: '***这里是代码链接***'
+                    },
+                    {
+                        name: 'Canvas轻画板2',
+                        keywords: 'Canvas、jQuery、移动端',
+                        description: '一个轻量级画板',
+                        previewLink: '***这里是预览链接***',
+                        codeLink: '***这里是代码链接***'
+                    },
+                    {
+                        name: 'Canvas轻画板3',
+                        keywords: 'Canvas、jQuery、移动端',
+                        description: '一个轻量级画板',
+                        previewLink: '***这里是预览链接***',
+                        codeLink: '***这里是代码链接***'
+                    },
+                    {
+                        name: 'Canvas轻画板4',
+                        keywords: 'Canvas、jQuery、移动端',
+                        description: '一个轻量级画板',
+                        previewLink: '***这里是预览链接***',
+                        codeLink: '***这里是代码链接***'
+                    }
+                ]
             }
         },
         methods: {
+            preview(e) {
+                console.log('这是预览')
+            },
             save(e) {
                 console.log('保存')
                 if (this.user.currentUser) {
                     //todo 已经登陆执行保存
                     this.saveResume()
                 } else {
-                    this.showIndex = false
-                    this.showLogin = true
+                    this.showEditArea = false
                     alert('您还未登录，请先登录!')
                 }
             },
@@ -42,19 +90,47 @@
             print(e) {},
             changeBackground(e) {},
             logout(e) {},
+            addSkill() {
+                console.log('add a skill')
+                this.resume.skills.push({
+                    name: '请填写技能名称',
+                    description: '请填写技能描述'
+                })
+            },
+            removeSkill(index) {
+                this.resume.skills.splice(index,1)
+            },
             saveResume() {
-                let {id} = AV.User.current()
-                let user = AV.Object.createWithoutData('User',id)
-                user.set('resume',this.resume)
+                let {
+                    id
+                } = AV.User.current()
+                let user = AV.Object.createWithoutData('User', id)
+                user.set('resume', this.resume)
                 user.save().then((data) => {
                     alert('保存成功！')
-                },(error) => {
+                }, (error) => {
                     alert('保存失败')
                 })
             },
             onEdit(key, data) {
-                console.log(this.resume)
-                this.resume[key] = data
+                console.log(typeof key)
+                console.log(key)
+                // key = 'skills[0].name'
+                let regex = /\[(\d+)\]/g
+                key = key.replace(regex,(match,number,index) => '.'+number)
+                needs = key.split('.')
+                console.log(needs)
+                let editData = this.resume
+                for (let i = 0; i < needs.length; i++) {
+                    if (i === needs.length-1) {
+                        console.log(needs[i])
+                        editData[needs[i]] = data
+                    } else {
+                        console.log(needs[i])
+                        editData = editData[needs[i]]
+                    }
+                }
+                //this.resume[key] = data
             },
             registered() {
                 if (this.user.pwd === this.user.confirmPwd) {
@@ -63,19 +139,18 @@
                     newUser.setEmail(this.user.email)
                     newUser.setPassword(this.user.pwd)
                     newUser.signUp().then((loginedUser) => {
-                        this.showIndex = true
-                        this.showLogin = false
+                        this.showEditArea = true
                         alert('注册成功，即将跳转至编辑页面！')
                         let currentUser = AV.User.current()
                         this.user.currentUser = currentUser
-                    },(error) => {
+                    }, (error) => {
                         alert(JSON.stringify(error))
                     })
                 } else {
                     alert('两次密码输入不一致，请重新输入')
                     return
                 }
-                Object.assign(this.user,{
+                Object.assign(this.user, {
                     name: '',
                     email: '',
                     pwd: '',
@@ -84,18 +159,17 @@
             },
             login() {
                 console.log('登录函数')
-                AV.User.logIn(this.user.name,this.user.pwd).then((loginedUser) => {
-                    this.showIndex = true
-                    this.showLogin = false
+                AV.User.logIn(this.user.name, this.user.pwd).then((loginedUser) => {
+                    this.showEditArea = true
                     let currentUser = AV.User.current()
                     this.user.currentUser = currentUser
                     alert('登录成功，即将跳转至编辑页面！')
-                },(error) => {
+                }, (error) => {
                     if (error.code === 211) {
                         alert('用户不存在，请先注册！')
                     }
                 })
-                Object.assign(this.user,{
+                Object.assign(this.user, {
                     name: '',
                     email: '',
                     pwd: '',
--- src/js/component.js
+++ src/js/component.js
@@ -2,9 +2,31 @@
     Vue.component('editable-span', {
         props: ['value'],
         template: `
-        <div class="template">
+        <div class="editable-span">
                 <span v-show="!editingName">{{value}}</span>
                 <input v-show="editingName" type="text" v-bind:value="value" @input="update">
+                <svg class="icon edit" aria-hidden="true"  @click="editingName = !editingName">
+                    <use xlink:href="#icon-edit"></use>
+                </svg>
+        </div>`,
+        data() {
+            return {
+                editingName: false
+            }
+        },
+        methods: {
+            update(e) {
+                console.log(e.target.value)
+                this.$emit('edit', e.target.value)
+            }
+        }
+    })
+    Vue.component('editable-textarea', {
+        props: ['value'],
+        template: `
+        <div class="editable-textarea">
+                <p v-show="!editingName">{{value}}</p>
+                <textarea v-show="editingName" type="text" v-bind:value="value" @input="update"></textarea>
                 <svg class="icon edit" aria-hidden="true"  @click="editingName = !editingName">
                     <use xlink:href="#icon-edit"></use>
                 </svg>
